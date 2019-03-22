3fdc2393817b2674a49f08d4a61769666c1ea818
Tianle97 <you@example.com>
2019-03-22 11:20:46 +0000

    finish first step and finish setup

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -21,6 +21,8 @@ module NavigationHelpers
     when /^the details page for "(.*)"$/i  # I am on the details page for "Star Wars"
       movie_path(Movie.find_by_title($1))
 
+    when /^the similar movies page for "(.*)"$/i  # I am on the similar movies page for "Star Wars"
+       search_directors_path(Movie.find_by_title($1))  
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
---
+++ rottenpotatoes/spec/movie_model_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+
+describe Movie do
+  context 'other movies by that director' do
+    it 'finds movies with same director' do
+        # YOUR TEST CODE HERE
+        # Remove the pending and fail lines to execute your test code
+        pending "Implement this test!"
+        fail
+    end        
+  end    
+
+  context 'no other movies by that director' do
+    it 'finds no other movies with same director' do
+        # YOUR TEST CODE HERE
+        # Remove the pending and fail lines to execute your test code
+        pending "Implement this test!"
+        fail
+    end
+  end
+end 
