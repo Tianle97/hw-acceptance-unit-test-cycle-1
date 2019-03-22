b87cba82765c02bc388af35a9a7bdafaeb4f0b4b
Tianle97 <you@example.com>
2019-03-22 10:22:13 +0000

    add directors and finish the last step in first scenario

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -15,6 +15,12 @@ module NavigationHelpers
 
     when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
 
+    when /^the edit page for "(.*)"$/i # I go to the edit page for "Alien"
+      edit_movie_path(Movie.find_by_title($1))      
+
+    when /^the details page for "(.*)"$/i  # I am on the details page for "Star Wars"
+      movie_path(Movie.find_by_title($1))
+
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
